da77b11a931683d9872979ad376eae6abb296a55
Tanzeebul-Tamim <tamim200091@gmail.com>
2023-05-22 20:10:24 +0600

    toy detail added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -25,11 +25,18 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const serviceCollection = client.db('fluffyFriends').collection('allToys');
+    const toyCollection = client.db('fluffyFriends').collection('allToys');
 
     app.get('/allToys', async(req, res) => {
-        const cursor = serviceCollection.find();
+        const cursor = toyCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/allToys/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await toyCollection.findOne(query);
         res.send(result);
     })
 
