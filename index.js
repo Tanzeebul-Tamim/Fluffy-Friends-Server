dba7bebe7cdd3499676a2f5852f8ddbe52770c62
Tanzeebul-Tamim <tamim200091@gmail.com>
2023-05-24 16:17:30 +0600

    completed

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
 
     const toyCollection = client.db("fluffyFriends").collection("allToys");
 
